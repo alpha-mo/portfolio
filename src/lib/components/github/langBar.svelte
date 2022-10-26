6c3e4f622a9b2d81af219d9e8e28afcfd4377ec1
MohamadOjail <mohamad.ojail@gmail.com>
2022-10-26 17:20:15 +0200

    Github ready to refactor

---
+++ src/lib/components/github/langBar.svelte
@@ -0,0 +1,55 @@
+<script>
+  import "$styles/lanBarColors.css";
+  import { getClassName, getPercentage } from "./utils";
+  export let langs = [{ lang: "other", value: 1 }];
+  let total = 0;
+  langs.forEach((l) => {
+    total = total + l.value;
+  });
+  const getWidth = (lang) => {
+    const pecentage = getPercentage(total, lang.value);
+    return `width: ${pecentage}%;`;
+  };
+</script>
+
+<div class="container">
+  <div class="bar">
+    {#each langs as lang}
+      <div class={getClassName(lang.lang)} style={getWidth(lang)} />
+    {/each}
+  </div>
+
+  <div class="names">
+    {#each langs as lang}
+      <p class={getClassName(lang.lang)}>{lang.lang}</p>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .container {
+    width: 100%;
+  }
+  .bar {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 5px;
+    border-radius: 5px;
+    overflow: hidden;
+  }
+  .names {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 20px;
+    gap: 10px;
+  }
+  .names p {
+    font-size: 14px;
+    background: none;
+  }
+  .bar div {
+    height: 10px;
+  }
+</style>
--- src/routes/github/+page.svelte
+++ src/routes/github/+page.svelte
@@ -1,1 +1,32 @@
-<h3>Github</h3>
+<script>
+  import Card from "$components/github/card.svelte";
+  const langs = [
+    { lang: "html", value: 500 },
+    { lang: "typescript", value: 250 },
+    { lang: "svelte", value: 1235 },
+    { lang: "handlebars", value: 835 },
+  ].sort((a, b) => {
+    return b.value - a.value;
+  });
+  export let data;
+</script>
+
+<div class="container">
+  {#each data.repos as repo}
+    <Card {repo} />
+  {/each}
+</div>
+
+<style>
+  .container {
+    display: flex;
+    width: 65%;
+    padding: 10px;
+    margin: 50px auto;
+    flex-wrap: wrap;
+    flex-direction: row;
+    gap: 15px;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
