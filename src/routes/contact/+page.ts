3cf63732fc5e43ed1d1b7b4f5a6b635a5488bbbd
MohamadOjail <mohamad.ojail@gmail.com>
2022-11-10 15:21:34 +0100

    logging

--- src/routes/contact/+page.ts
+++ src/routes/contact/+page.ts
@@ -11,39 +11,39 @@ const res = await fetch('http://localhost:4000', {
 const result = await res.json()
 console.log(result)
 
-export const actions: Actions = {
-  default: async ({ request, fetch }) => {
-    console.log('actions in +page.ts invoked')
-    const formData = await request.formData()
-    const { name, message, email } = fromForm(formData)
-    let result: {}
+// export const actions: Actions = {
+//   default: async ({ request, fetch }) => {
+//     console.log('actions in +page.ts invoked')
+//     const formData = await request.formData()
+//     const { name, message, email } = fromForm(formData)
+//     let result: {}
 
-    if (areOk({ name, email, message })) {
-      const res = await fetch('http://localhost:4000', {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email, message }),
-      })
-      result = await res.json()
-    } else {
-      return {
-        success: false,
-        msg: 'Bad Request',
-        nameErr: !isOk(name, false),
-        emailErr: !isOk(email, true),
-        messageErr: !isOk(message, false),
-        name,
-        email,
-        message,
-      }
-    }
+//     if (areOk({ name, email, message })) {
+//       const res = await fetch('http://localhost:4000', {
+//         method: 'POST',
+//         credentials: 'same-origin',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ name, email, message }),
+//       })
+//       result = await res.json()
+//     } else {
+//       return {
+//         success: false,
+//         msg: 'Bad Request',
+//         nameErr: !isOk(name, false),
+//         emailErr: !isOk(email, true),
+//         messageErr: !isOk(message, false),
+//         name,
+//         email,
+//         message,
+//       }
+//     }
 
-    return {
-      success: true,
-      result,
-    }
-  },
-}
+//     return {
+//       success: true,
+//       result,
+//     }
+//   },
+// }
